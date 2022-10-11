40021f4513bb1d49ff2c9e686df2adf9f186f469
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2022-10-11 23:05:27 +0300

    Добавление работы с логгером

--- java_homeworks/src/homework_2/Main.java
+++ java_homeworks/src/homework_2/Main.java
@@ -2,11 +2,24 @@ package homework_2;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
+        //Путь к директории для теста
         File directory = new File("/Users/mihailungureanu/Desktop/Test");
+
+        //Запись информации в документ directories.txt
         task1.WriteInFile(task1.getDirectory(directory) , "directories.txt");
+
+        //Работа с логгером
+        Logger logger = Logger.getLogger(task1.class.getName());
+        ConsoleHandler ch = new ConsoleHandler();
+        FileHandler fh = new FileHandler("log.txt");
+        logger.addHandler(ch);
+        logger.addHandler(fh);
     }
 }
