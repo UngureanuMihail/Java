fa85efcd275ee48fc7c706c479e5d3e77e34f62b
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2022-10-11 22:37:16 +0300

    Точка входа

--- java_homeworks/src/homework_2/Main.java
+++ java_homeworks/src/homework_2/Main.java
@@ -1,2 +1,13 @@
-package homework_2;public class Main {
+package homework_2;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        File directory = new File("/Users/mihailungureanu/Desktop/Test");
+        var get = task1.getDirectory(directory);
+
+    }
 }
