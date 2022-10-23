e57874ea7771050e435cd439bbdab2efd9d981b7
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2022-10-23 16:20:32 +0300

    Работа с средним арифметическим числом

--- JavaHW/src/lesson3/taks2.java
+++ JavaHW/src/lesson3/taks2.java
@@ -1,16 +1,24 @@
 package lesson3;
-
 import java.util.*;
-import java.util.stream.IntStream;
 
 public class taks2 {
+    static List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 10, 13);
 
 
-    public static void Math() {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+    public static void math() {
         Integer max = Collections.max(list);
         System.out.println("Максимальное число: " + max);
         Integer min = Collections.min(list);
         System.out.println("Минимальное число: " + min);
+    }
+
+    public static void average (){
+        double avName = 0;
+        if (list.size() > 0){
+            for (int i = 0; i <list.size() ; i++) {
+                avName += list.get(i);}
+        }
+        System.out.println(avName / list.size());
     }
 }
