8e0f4963cdaba3c4a7fdf96c173aa5004b63dc7f
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2022-10-31 19:16:41 +0200

    Channge main file

--- JavaHW/src/lesson5/main.java
+++ JavaHW/src/lesson5/main.java
@@ -1,2 +1,10 @@
-package lesson5;public class main {
+package lesson5;
+
+public class main {
+    public static void main(String[] args) {
+        String[] clients = {"Michael-123", "John-345", "George-2233"};
+        task5.PhoneBook(clients);
+    }
 }
+
+
